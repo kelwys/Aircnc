62b3195146d5fddfd1c8d21099d2449782fc3143
Kelwy Oliveira <kelwys@ucl.br>
2020-01-13 23:15:44 -0300

    Adiciona rotas para o backend

---
+++ backend/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const multer = require('multer');
+const UploadConfig = require('./config/upload');
+
+const SessionControler = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+
+const routes = express.Router();
+const upload = multer(UploadConfig);
+
+routes.post('/session', SessionControler.store);
+routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+
+module.exports = routes;
